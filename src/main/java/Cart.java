8fe309e38716630b1adbba24cde73b58e9b21e0d
Rasheeda <krashee@thoughtworks.com>
2022-03-25 15:39:16 +0530

    first commit

---
+++ src/main/java/Cart.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Cart {
+
+    private List<CartItem> cartItems;
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+}
